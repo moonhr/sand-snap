18277f504a075e29ee1bfa1786455175beebc235
moonhr <moonhr153@gmail.com>
2024-08-23 16:47:49 +0900

    Feat | 유저 생성 dto(유효성검사 포함)

---
+++ packages/server/src/dtos/create-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsEmail, IsNotEmpty, IsString, IsOptional } from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  passwordHash: string;
+
+  @IsOptional()
+  @IsString()
+  profilePicture: string;
+}
