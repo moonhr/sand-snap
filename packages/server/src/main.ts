cb46087e2c3749116f8120f10277bc80bbf84e79
moonhr <moonhr153@gmail.com>
2024-08-23 16:47:30 +0900

    Fix | 유효성 dto를 사용하기 위한 전역 파이프 설정 추가

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+//유효성dto를 활성화하기 위한 전역 파이프 설정 ValidationPipe
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3001);
 }
 bootstrap();
