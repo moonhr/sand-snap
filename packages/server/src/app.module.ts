201a05e26a05f18b069649746b07d4f5dc42482e
moonhr <moonhr153@gmail.com>
2024-08-23 16:47:11 +0900

    Fix | user.module.ts 주입

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -4,12 +4,13 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGODB_URI), // MongoDB 연결 설정
-    // 다른 모듈들 추가
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
