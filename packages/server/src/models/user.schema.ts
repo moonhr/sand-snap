ffbbd981df55d6252e3c26f58af78fc845fbb481
moonhr <moonhr153@gmail.com>
2024-08-23 15:56:09 +0900

    Feat | user스키마

---
+++ packages/server/src/models/user.schema.ts
@@ -0,0 +1,22 @@
+//* 여러 모듈에서 공유하기 위해 모델을 따로 정의한 스키마.
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+//UserDocument: User 클래스와 Document 타입을 결합하여, Mongoose의 문서 기능을 사용.
+export type UserDocument = User & Document;
+//timestamps를 설정하여 createdAt과 updatedAt를 자동으로 관리한다.
+@Schema({ timestamps: true })
+export class User {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ required: true, unique: true })
+  email: string;
+
+  @Prop({ required: true })
+  passwordHash: string;
+
+  @Prop({ required: true })
+  profilePicture: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
--- packages/server/src/modules/user/user.schema.ts
+++ packages/server/src/modules/user/user.schema.ts
@@ -1,10 +1,8 @@
+//* 모듈 안에 정의되어 해당 모듈의 디렉토리에서 관리됨. 종속성이 줄어듦.
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
 import { Document } from 'mongoose';
-
 //UserDocument: User 클래스와 Document 타입을 결합하여, Mongoose의 문서 기능을 사용.
 export type UserDocument = User & Document;
-
 //timestamps를 설정하여 createdAt과 updatedAt를 자동으로 관리한다.
 @Schema({ timestamps: true })
 export class User {
@@ -20,5 +18,4 @@ export class User {
   @Prop({ required: true })
   profilePicture: string;
 }
-
 export const UserSchema = SchemaFactory.createForClass(User);
